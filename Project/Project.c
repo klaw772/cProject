7d5655ed306b3d0b801f62e6df5e0f220792a082
James Blash <j.w.blash@gmail.com>
2018-05-04 09:00:53 -0400

    added functions

--- Project/Project.c
+++ Project/Project.c
@@ -3,6 +3,7 @@
 
 void blackjack();
 void war();
+int gameCheck; //global variable for re-calling the same game they had initially played
 
 int main() {
   system("@cls||clear");
@@ -16,12 +17,14 @@ int main() {
     case 1:
       system("@cls||clear");
       printf("\n\n\n\n");
-      printf("\t\t\t\t\t\t   Let's play Blackjack!");
+      printf("\t\t\t\t\t\t   Let's play Blackjack!\n");
+      blackjack(); //calls the blackjack function
       break;
     case 2:
       system("@cls||clear");
       printf("\n\n\n\n");
-      printf("\t\t\t\t\t\t   Let's play War!");
+      printf("\t\t\t\t\t\t   Let's play War!\n");
+      war(); //calls the war function
       break;
   }
   while (game != 1 && game != 2)
@@ -41,8 +44,27 @@ int main() {
       break;
     case 'n':
     case 'N':
-      printf("Let's keep going!");
+      printf("Let's keep going!\n"); // unfortunately this code doesn't loop, since after it re-calls the game they played, the main function ends.
+                                     // So it currently just calls it once more if they say they want to keep playing
+      if (gameCheck == 1) //if they already called the blackjack function, then gameCheck = 1
+        blackjack(); // since gameCheck = 1, it re-calls the blackjack function
+      else //if they had called the war function though, gameCheck = 2
+        war(); // since gameCheck != 1, it re-calls the war function
       break;
   }
   return 0;
+}
+
+// A barebones function that sets global variable gameCheck to 1
+// and prints a successful game played message
+void blackjack(){}
+  gameCheck = 1;
+  printf("Believe it or not, you just played Blackjack!\n");
+}
+
+// A barebones function that sets global variable gameCheck to 2
+// and prints a successful game played message
+void war(){
+  gameCheck = 2;
+  printf("Believe it or not, you just played War!\n");
 }
